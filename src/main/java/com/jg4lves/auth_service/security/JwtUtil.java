b2e2f4bf5f364956665afc52ab0c44d4abee8a25
jgalves <hermesjgcode@gmail.com>
2026-04-23 15:12:31 -0300

    feat(auth): add JWT utility and authentication filter

---
+++ src/main/java/com/jg4lves/auth_service/security/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.jg4lves.auth_service.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey key = Keys.hmacShaKeyFor("minha-chave-secreta-super-segura-123456".getBytes());
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+
+    public String validateToken(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
